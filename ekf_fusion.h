6cb452acf3a5bf4b6eb4f7e53fee874464afc143
daofeng <9779716+du-daofeng@user.noreply.gitee.com>
2022-06-13 15:54:17 +0800

    encoder imu fusion

--- ekf_fusion.h
+++ ekf_fusion.h
@@ -5,7 +5,7 @@
 #include <Eigen/Dense>
 #include <cmath>
 
-namespace imu
+namespace ekf
 {
 
 template<typename T>
@@ -34,6 +34,18 @@ public:
 
 	void predict( const Vector3 &u_now, const DataType time_now )
 	{
+		if( is_init == false ){
+			time_pre = time_now;
+			u_pre = u_now;
+			
+			is_init = true;
+			
+			std::cout<<"initialize !"<<std::endl;			
+
+			return;
+		}
+		std::cout<<"predict "<<std::endl;		
+
 		// 1. state prediction
 		Matrix2 q_rotation_pre;
 		q_rotation_pre << ::cos(x_pre[0]), -::sin(x_pre[0]),
@@ -48,7 +60,7 @@ public:
 		q_rotation_now << ::cos(x_now[0]), -::sin(x_now[0]),
 				  ::sin(x_now[0]),  ::cos(x_now[0]);
 	
-		Vector2 a_m = 0.5 * ( q_rotation_pre * Vector2( u_pre[0], u_pre[1] ),
+		Vector2 a_m = 0.5 * ( q_rotation_pre * Vector2( u_pre[0], u_pre[1] ) 
 				    + q_rotation_now * Vector2( u_now[0], u_now[1] ) );
 	
 		x_now[1] = x_pre[1] + a_m[0] * delta_t; // v_x
@@ -74,35 +86,50 @@ public:
 		
 		// ---------- update the value 
 		P_pre = P_now;
+		
+		std::cout<<"predict end"<<std::endl;
 	}
 
 	void update( const Vector3 &z ) // theta, s, v
 	{
+		if( is_init == false ){
+			return;
+		}
+		std::cout<<"update"<<std::endl;
+
 		// measurement function
 		Vector3 h_measurement;
 		h_measurement[0] = x_now[0] - x_pre[0]; // theta
-		DataType tmp = ::cos( x_pre[0] + 0.5 * h_measurement[0] ) + ::sin( x_pre[0] + 0.5 * h_measurement[0] ) ;
+
+		DataType cos_tmp = ::cos( x_pre[0] + 0.5 * h_measurement[0] );
+		DataType sin_tmp = ::sin( x_pre[0] + 0.5 * h_measurement[0] ) ;
+		DataType tmp = cos_tmp + sin_tmp;
 		h_measurement[1] = ( x_now[3] - x_pre[3] + x_now[4] - x_pre[4] ) / ( tmp ); // s
 		h_measurement[2] = ::sqrt( x_now[1] * x_now[1] + x_now[2] * x_now[2] ); // v
 	
 		// error
 		Vector3 error = z - h_measurement;
-		
+		std::cout<<"error = "<<std::endl<<error<<std::endl;		
+
 		// measurement Jacobian Matrix
 		Matrix3x5 H;
-		H <<	1, 							0,                 0,              0,       0;
-		     ( ::sin( x_pre[0] + 0.5 * h_measurement[0] )
-		       -::cos( x_pre[0] + 0.5 * h_measurement[0] ) ) 
-		       * ( x_now[3] - x_pre[3] + x_now[4] - x_pre[4] ) * 0.5,   0,                 0,             1 / tmp, 1 / tmp,
-			0,     		                                      1 / h_measurement, 1 / h_measurement, 0,       0;
+		DataType h_1_0 = ( sin_tmp - cos_tmp ) * ( x_now[3] - x_pre[3] + x_now[4] - x_pre[4] ) * 0.5;
+		H << 1,                 0,                    0,                    0,       0,
+		     h_1_0 / tmp * tmp, 0,                    0,                    1 / tmp, 1 / tmp,
+		     0,                 1 / h_measurement[2], 1 / h_measurement[2], 0,       0;
+
+		std::cout<<"H = "<<std::endl<<H<<std::endl;	
+	
 		// Kalman gain
-		Matrix5 K_tmp = H * P_now * H.transpose() + R;
+		Matrix3 K_tmp = H * P_now * H.transpose() + R;
 		Matrix5x3 K = P_now * H.transpose() * K_tmp.inverse();
 
 		// state update
 		x_now += K * error;
 		// state covarince update
 		P_now = ( Matrix5::Identity() - K * H ) * P_now;
+	
+		std::cout<<"update end"<<std::endl;
 	}
 
 	const Vector5& getStateX() const
@@ -110,6 +137,16 @@ public:
 		return x_now;
 	}
 	
+	void setStateNoiseQ( const Matrix5 &Q_ ) 
+	{
+		Q = Q_;
+	}	
+	
+	void setMeasurementNoise( const Matrix3 &R_ )
+	{
+		R = R_;
+	}
+
 private:
 	// control vector at k-1 moment, ( a_x, a_y, w )
 	Vector3 u_pre = Vector3::Zero();
@@ -132,6 +169,8 @@ private:
 	// previous time moment
 	DataType time_pre = 0;
 	
+	// 
+	bool is_init = false;
 };
 
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include "ekf_fusion.h"
+#include "file_read.h"
+
+#include <vector>
+#include <iostream>
 
 int main()
 {
 	std::cout<<"--------------------- EKF --------------------"<<std::endl;
 
-	imu::EKF<double> ekf;
+	ekf::EKF<double> ekf;
 	
+	ekf::DataRead data_read;
+	data_read.openFile( "test_data1.txt" );
+	
+	Eigen::Matrix<double, 6, 1> encoder_data;
+	Eigen::Matrix<double, 4, 1> imu_data;
+	int count = 0;
+	while( !data_read.endOfFile() ){
+		std::cout<<"frame count : "<<count <<std::endl;
+		int ret = data_read.readFrameData( encoder_data, imu_data );
+		if( ret == 0 ){ // encoder data
+			
+			Eigen::Vector3d z;
+			z(0) = encoder_data(1);
+			z(1) = encoder_data(2);
+			z(2) = encoder_data(3);
+			
+			ekf.update( z );
+			
+			Eigen::Matrix<double, 5, 1> x_now = ekf.getStateX();
+			std::cout<<"X = "<<std::endl<<x_now<<std::endl;
+		}
+		else if( ret == 1 ){ // imu data
+			Eigen::Vector3d u_now;
+			u_now(0) = imu_data(1) - (-0.0078120);
+			u_now(1) = imu_data(2) - (0.027588);
+			u_now(2) = imu_data(3) - (-4.549618);
+			double time_now = imu_data(0) / 1000;
+	
+			ekf.predict( u_now, time_now );
+		
+		}
+		count ++;
+	}
+	data_read.closeFile();
+
+	
+
 	return 0;
 }
